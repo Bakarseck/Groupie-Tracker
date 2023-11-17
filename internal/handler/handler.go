e02dfe877e3705f45f4037722b1771d89eac0b55
cheikh-nakamoto <dioufmounirou76@gmail.com>
2023-11-17 10:49:11 +0000

    Test HTML CSS JS

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -17,12 +17,16 @@ func render(w http.ResponseWriter, tmpl string, value interface{}) {
 
 func TemplateCache()(map[string]*template.Template, error) {
 	cache := make(map[string]*template.Template
-	pages,err := filepath.ParseGlob("./templates/*.tmpl")
+	pages,err := filepath.Glob("./templates/*.tmpl")
 	if err != nil {
 		log.Fatal("page not found")
 	}
 	for page := range pages {
 		name := filepath.Base(page)
-		tmpl := template.Must(template.Parsefiles(page)
+		tmpl := template.Must(template.Parsefiles(page))
+		layout, err := filepath.Glob("/templates/layout/*.tmpl")
+		if err != nil {
+			
+		}
 	}
 }
